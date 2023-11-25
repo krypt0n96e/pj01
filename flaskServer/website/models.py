0a98ae6e5616d88de8ea4044957f8de962689796
Test2 <your.email@example.com>
2023-11-25 20:50:42 +0700

    git push origin main

--- flaskServer/website/models.py
+++ flaskServer/website/models.py
@@ -7,6 +7,7 @@ class data1(db.Model):
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=datetime.utcnow())
     device_id = db.Column(db.Integer, db.ForeignKey('device1.id'))
+    
 
 class device1(db.Model):
     id = db.Column(db.Integer, primary_key=True)
